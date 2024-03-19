e83ccab666d86bee84190d7cfeb281e4f60058ce
GitHubNurislam <vipbaron06@gmail.com>
2024-03-19 15:45:33 +0600

    main

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { IoBagRemoveOutline } from "react-icons/io5";
+
 
 const Home = () => {
     return (
-        <>
-
-        </>
+        <main>
+            <section className="main">
+                <div className="container">
+                    <p className="main__desc">
+                        Senior Frontend Developer (Vue.Js)
+                    </p>
+                    <p className="main__price">
+                        4 000 $
+                    </p>
+                    <p className="main__exp">
+                        <IoBagRemoveOutline />
+                        Опыт более 6 лет
+                    </p>
+                    <button className="main__home">
+                        Можно из дома
+                    </button>
+                    <button className="main__btn">
+                        Откликнуться
+                    </button>
+                </div>
+            </section>
+        </main>
     );
 };
 
