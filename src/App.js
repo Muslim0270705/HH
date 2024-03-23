c5577a40721326c3a42def41304c33dab5f55c0b
Muslim Mageza <muslimmageza@MacBook-Pro-Muslim.local>
2024-03-23 19:10:54 +0600

    fix bag

--- src/App.js
+++ src/App.js
@@ -5,25 +5,16 @@ import Header from "./Layout/Header/Header";
 import "./styles/style.scss"
 import {Routes,Route} from "react-router-dom";
 import Layout from "./Layout/Layout";
-<<<<<<< HEAD
 import Register from "./pages/Auth/Register/Register";
-=======
 import Help from "./pages/Help/Help";
-
->>>>>>> 64ccbef7e1a8c8eb5584e465e43474d3d8bf62d5
-
 function App() {
   return (
     <Routes>
         <Route path={"/"} element={<Layout/>}>
             <Route path={"/"} element={<Home/>}/>
-<<<<<<< HEAD
-        </Route>
-        <Route path={"/register"} element={<Register/>}/>
-=======
             <Route path={"/help"} element={<Help/>}/>
         </Route>
->>>>>>> 64ccbef7e1a8c8eb5584e465e43474d3d8bf62d5
+        <Route path={"/register"} element={<Register/>}/>
     </Routes>
   );
 }
